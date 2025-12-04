7818130323063709e9c5f7b0b594988b18cd24ee
tiky! <ytegorezem@gmail.com>
2025-12-04 16:33:08 +1000

    Fix compilation issues in debug and sign handling

--- src/main/java/com/watchbox/maniac/KillerSignListener.java
+++ src/main/java/com/watchbox/maniac/KillerSignListener.java
@@ -20,6 +20,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Handles right-click interactions with the Killer Sign and provides the Maniac ability UI.
